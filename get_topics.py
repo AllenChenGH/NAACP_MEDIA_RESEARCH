073e74d8c6b524a73a41fe47c7eb8f8e9fb5da83
AllenChenGH <Allen_Cyfff@163.com>
2019-12-08 15:02:02 -0500

    Add files via upload

--- get_topics.py
+++ get_topics.py
@@ -1,20 +1,23 @@
-from jieba.analyse import *
 import pandas as pd
-import jieba
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
+import pyLDAvis
+import pyLDAvis.sklearn
 
 
-def print_top_words(model, feature_names, n_top_words, f):
+def print_top_words(model, feature_names, n_top_words, file_name):
+    f = open(file_name, 'w+')
     for topic_idx, topic in enumerate(model.components_):
         print("Topic #%d:" % topic_idx, file=f)
         print(" ".join([feature_names[i]
                         for i in topic.argsort()[:-n_top_words - 1:-1]]), file=f)
-    print(file=f)
+    f.close()
+    print()
 
-def get_topics(file, f):
+
+def get_topics(file, file2):
     df = pd.read_csv(file)
-    print('processing', file.replace('..//relevant data//', ''),", may take a long time")
+    print('Processing ',file.replace('..//relevant data//', ''),". It may take a long time.")
     stop_words = ['said', 'says', 'boston', 'globe', 'men', 'women', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
                   'Friday', 'Saturday', 'Sunday', 'Boston', 'people', 'dorchester', 'walsh', 'Dorchester', 'Roxbury',
                   'Walsh', 'Mattapan', 'Dudley', 'Park', 'new', 'city', 'City', 'time', 'Square', 'district', 'year',
@@ -30,36 +33,32 @@ def get_topics(file, f):
                                     min_df=10)
     tf = tf_vectorizer.fit_transform(df['text'])
 
-    # number of clusters
-    n_clusters = 5
-    lda = LatentDirichletAllocation(n_components=n_clusters, max_iter=50,
+    # cluster number
+    lda = LatentDirichletAllocation(n_components=5, max_iter=50,
                                     learning_method='online',
                                     learning_offset=50.,
                                     random_state=0)
     lda.fit(tf)
 
-    # number of topics
+    # topic number
     n_top_words = 5
     tf_feature_names = tf_vectorizer.get_feature_names()
-    print_top_words(lda, tf_feature_names, n_top_words, f)
+    print_top_words(lda, tf_feature_names, n_top_words, file2)
+    data = pyLDAvis.sklearn.prepare(lda, tf, tf_vectorizer)
+    pyLDAvis.show(data)
 
     return
 
 
 if __name__ == '__main__':
-    f = open('5topics_black.txt', 'w+')
-    get_topics('classified data//bostonglobe2014.csv', f)
-    get_topics('classified data//bostonglobe2015.csv', f)
-    get_topics('classified data//bostonglobe2016.csv', f)
-    get_topics('classified data//bostonglobe2017.csv', f)
-    get_topics('classified data//bostonglobe2018.csv', f)
-    f.close()
+    get_topics('classified data//bostonglobe2014.csv', '5topics_all.txt')
+    # get_topics('classified data//bostonglobe2015.csv', '5topics_all.txt')
+    # get_topics('classified data//bostonglobe2016.csv', '5topics_all.txt')
+    # get_topics('classified data//bostonglobe2017.csv', '5topics_all.txt')
+    # get_topics('classified data//bostonglobe2018.csv', '5topics_all.txt')
 
-    f = open('5topics_all.txt', 'w+')
-    get_topics('classified data//bostonglobe2014.csv', f)
-    get_topics('classified data//bostonglobe2015.csv', f)
-    get_topics('classified data//bostonglobe2016.csv', f)
-    get_topics('classified data//bostonglobe2017.csv', f)
-    get_topics('classified data//bostonglobe2018.csv', f)
-    f.close()
-
+    get_topics('classified data//bostonglobe2014.csv', '5topics_black.txt')
+    # get_topics('classified data//bostonglobe2015.csv', '5topics_black.txt')
+    # get_topics('classified data//bostonglobe2016.csv', '5topics_black.txt')
+    # get_topics('classified data//bostonglobe2017.csv', '5topics_black.txt')
+    # get_topics('classified data//bostonglobe2018.csv', '5topics_black.txt')
